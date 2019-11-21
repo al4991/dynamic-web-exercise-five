f3e2b71d8a5600d5d70f3e17e8452a10a82903e3
Andrew Lucero <al4991@nyu.edu>
2019-11-21 15:02:36 -0500

    Initialized Repo, created simple form

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+export default function Login() { 
+    return <div> 
+        <form>
+            <label for="loginEmail">Email</label>
+            <input type="email" name="loginEmail" placeholder="email"/>
+            <label for="loginPassword">Password</label>
+            <input type="email" name="loginPassword" placeholder="password"/>
+            <button>Log In</button>
+        </form> 
+    </div>
+}
