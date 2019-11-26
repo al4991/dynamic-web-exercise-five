c7d89fe90b262ccf079982aa6034a6f8834716b0
Andrew Lucero <al4991@nyu.edu>
2019-11-26 15:08:13 -0500

    Created a simple form. testing netlify deploy

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,13 +1,15 @@
 import React from 'react'; 
 
-export default function Login() { 
-    return <div> 
-        <form>
-            <label for="loginEmail">Email</label>
-            <input type="email" name="loginEmail" placeholder="email"/>
-            <label for="loginPassword">Password</label>
-            <input type="email" name="loginPassword" placeholder="password"/>
-            <button>Log In</button>
-        </form> 
-    </div>
+export default function LoginForm() { 
+    return ( 
+        <div> 
+            <form>
+                <label for="loginEmail">Email</label>
+                <input type="email" name="loginEmail" placeholder="email"/>
+                <label for="loginPassword">Password</label>
+                <input type="email" name="loginPassword" placeholder="password"/>
+                <button>Log In</button>
+            </form> 
+        </div>
+    )
 }
